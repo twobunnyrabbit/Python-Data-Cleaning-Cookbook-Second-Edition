e6aacd2214cee2dbaba5623c726a784af1e7870c
Vincent Ngian <twobunnyrabbit@gmail.com>
2025-10-15 14:24:16 +1100

    reading excel files

--- ch_01.py
+++ ch_01.py
@@ -8,7 +8,13 @@ app = marimo.App(width="medium")
 def _():
     import marimo as mo
     import pandas as pd
-    return (pd,)
+    return mo, pd
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Import CSV file""")
+    return
 
 
 @app.cell
@@ -41,8 +47,150 @@ def _(df):
 
 
 @app.cell
-def _(df):
-    df['country'].value_counts().pipe()
+def _(df, pd):
+    df_num_obs_country = pd.DataFrame(
+        {
+            'n': df['country'].value_counts(),
+            'prop': df['country'].value_counts(normalize=True).values
+        }
+    )
+    df_num_obs_country
+    return (df_num_obs_country,)
+
+
+@app.cell
+def _(df, df_num_obs_country):
+    # List the top five countries with the most observations
+    top_five_countries = df_num_obs_country[:5].index.to_list()
+    # top_five_countries
+    is_in_top_five = df['country'].isin(top_five_countries)
+    # is_in_top_five[:10]
+    df_top_five = df[is_in_top_five]
+    df_top_five.shape
+    return (df_top_five,)
+
+
+@app.cell
+def _(df_top_five):
+    df_gb_country_station = df_top_five.groupby(['country'])['station'].value_counts()
+    df_gb_country_station
+    return (df_gb_country_station,)
+
+
+@app.cell
+def _(df_gb_country_station):
+    df_gb_country_station.index
+    return
+
+
+@app.cell
+def _(df_gb_country_station):
+    # Get top 5 stations for each country
+    top_5_stations_per_country = df_gb_country_station.groupby(level=0).head(5)
+    top_5_stations_per_country
+    return (top_5_stations_per_country,)
+
+
+@app.cell
+def _(top_5_stations_per_country):
+    top_5_stations_per_country.reset_index()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Import Excel file""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(pd):
+    df_xl = pd.read_excel(
+        '1. ImportingTabularData/data/GDPpercapita22b.xlsx', 
+        sheet_name="OECD.Stat export", 
+        skiprows=4, 
+        skipfooter=1,
+        usecols="A,C:W"
+    
+    )
+    return (df_xl,)
+
+
+@app.cell
+def _(df_xl):
+    df_xl
+    return
+
+
+@app.cell
+def _(df_xl):
+    df_xl.columns
+    return
+
+
+@app.cell
+def _(df_xl):
+    # rename column Year to metro
+    df_xl.rename(columns={'Year': 'metro'}, inplace=True)
+    return
+
+
+@app.cell
+def _(df_xl):
+    starts_with_space = df_xl.metro.str.startswith(' ')
+    starts_with_space
+    return
+
+
+@app.cell
+def _(df_xl):
+    df_xl['metro'] = df_xl['metro'].str.strip()
+    return
+
+
+@app.cell
+def _(df_xl, pd):
+    # convert all columsn to numeric values
+    for col in df_xl.columns[1:]:
+        df_xl[col] = pd.to_numeric(df_xl[col], errors='coerce')
+        df_xl.rename(columns={col:'pcGDP' + col}, inplace=True)
+    return
+
+
+@app.cell
+def _(df_xl):
+    # drop na columns only if all rows have na
+    df_xl.dropna(subset=df_xl.columns[1:], how='all', inplace=True)
+    return
+
+
+@app.cell
+def _(df_xl):
+    df_xl.shape
+    return
+
+
+@app.cell
+def _(df_xl):
+    # set index using metro column
+    df_xl.set_index('metro', inplace=True)
+    return
+
+
+@app.cell
+def _(df_xl):
+    original_index = df_xl.index
+    return (original_index,)
+
+
+@app.cell
+def _(original_index):
+    original_index.to_list()
+    return
+
+
+@app.cell
+def _():
     return
 
 
